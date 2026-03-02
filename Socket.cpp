a8b6752197577798db97ecfd2d3c0684b0646ac5
lullabyeoytl <2199288762@qq.com>
2026-03-02 15:52:39 +0800

    feat(socket): wrap socket class

---
+++ Socket.cpp
@@ -0,0 +1,67 @@
+#include "Socket.hpp"
+#include <sys/socket.h>
+#include <netinet/tcp.h>
+#include "InetAddress.hpp"
+#include "Logger.hpp"
+
+void Socket::bindAddress(const InetAddress& localaddr)
+{
+    if (::bind(sockfd_, (sockaddr*)localaddr.getSockAddr(), sizeof(struct sockaddr_in)) != 0)
+    {
+        // handle error
+        LOG_FATAL("bind sockfd:%d fail", sockfd_);
+    }
+}
+
+void Socket::listen()
+{
+    if (::listen(sockfd_, SOMAXCONN) < 0)
+    {
+        // handle error
+        LOG_FATAL("listen sockfd:%d fail", sockfd_);
+    }
+}
+
+int Socket::accept(InetAddress* peeraddr)
+{
+    sockaddr_in addr;
+    socklen_t addrlen = sizeof(addr);
+    int connfd = ::accept(sockfd_, (sockaddr*)&addr, &addrlen);
+    if (connfd >= 0)
+    {
+        peeraddr->setSockAddr(addr);
+    }
+    return connfd;
+}
+
+void Socket::shutdownWrite()
+{
+    if (::shutdown(sockfd_, SHUT_WR) < 0)
+    {
+        LOG_ERROR("shutdownWrite error");
+    }
+}
+
+void Socket::setTcpNoDelay(bool on)
+{
+    int optval = on ? 1 : 0;
+    ::setsockopt(sockfd_, IPPROTO_TCP, TCP_NODELAY, &optval, sizeof(optval));
+}
+
+void Socket::setReuseAddr(bool on)
+{
+    int optval = on ? 1 : 0;
+    ::setsockopt(sockfd_, SOL_SOCKET, SO_REUSEADDR, &optval, sizeof(optval));
+}
+
+void Socket::setReusePort(bool on)
+{
+    int optval = on ? 1 : 0;
+    ::setsockopt(sockfd_, SOL_SOCKET, SO_REUSEPORT, &optval, sizeof(optval));
+}
+
+void Socket::setKeepAlive(bool on)
+{
+    int optval = on ? 1 : 0;
+    ::setsockopt(sockfd_, SOL_SOCKET, SO_KEEPALIVE, &optval, sizeof(optval));
+}
